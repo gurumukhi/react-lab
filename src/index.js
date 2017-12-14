84f57582a2a7285134bae95742d08ca86f1f3f89
Ram Dayal Vaishnav <Ram@Tokirs-MacBook-Air.local>
2017-12-15 02:26:10 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Clock extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {time: new Date()};
+  }
+
+  componentDidMount () {
+    this.timerId = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timerId);
+  }
+
+  tick () {
+    this.setState({
+      time: new Date()
+    });
+  }
+
+  render () {
+    return <div> {this.state.time.toLocaleTimeString()} </div>;
+  }
+}
+
+ReactDOM.render(
+  <Clock />,
+  document.querySelector('#root')
+);
